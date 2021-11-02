2b18a4e9d996b1d8f66f503b366c7e040d4254e3
lucaskrespi <lucas.krespi@gmail.com>
2021-11-01 20:49:19 -0400

    Main menu scene created / main menu button manager created

---
+++ GAME_PROD_V_11154/Assets/StartMenu/ButtonManager.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Button startButton, highScoreButton, exitButton;
+    public Image selectedButtonFlag;
+
+    Vector3 initial_pos;
+
+    float buttonGoal_pos;
+
+    private bool moveStart, moveExit, moveHighScore;
+
+
+    void Start()
+    {
+        initial_pos = selectedButtonFlag.transform.position;
+
+        startButton.Select();
+
+        startButton.onClick.AddListener(StartButtonClick);
+        exitButton.onClick.AddListener(ExitButtonClick);
+        highScoreButton.onClick.AddListener(HighScoreButtonClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        buttonGoal_pos = selectedButtonFlag.transform.position.y;
+
+        if (EventSystem.current.currentSelectedGameObject == startButton.gameObject)
+        {
+            Vector3 goal_pos = startButton.transform.position;
+
+            selectedButtonFlag.transform.position = new Vector3(Mathf.Lerp(initial_pos.x, goal_pos.x, 0.1f), initial_pos.y, initial_pos.z);
+
+            initial_pos = selectedButtonFlag.transform.position;
+        }
+
+        if (EventSystem.current.currentSelectedGameObject == highScoreButton.gameObject)
+        {
+            Vector3 goal_pos = highScoreButton.transform.position;
+
+            selectedButtonFlag.transform.position = new Vector3(Mathf.Lerp(initial_pos.x, goal_pos.x, 0.1f), initial_pos.y, initial_pos.z);
+           
+            initial_pos = selectedButtonFlag.transform.position;
+        }
+
+        if (EventSystem.current.currentSelectedGameObject == exitButton.gameObject)
+        {
+            Vector3 goal_pos = exitButton.transform.position;
+
+            selectedButtonFlag.transform.position = new Vector3(Mathf.Lerp(initial_pos.x, goal_pos.x, 0.1f), initial_pos.y, initial_pos.z);
+
+            initial_pos = selectedButtonFlag.transform.position;
+        }
+
+        if (moveStart && startButton.transform.position.y < buttonGoal_pos)
+        {
+            whenClicked(startButton);
+        }
+
+        if (moveExit && exitButton.transform.position.y < buttonGoal_pos)
+        {
+            whenClicked(exitButton);
+        }
+
+
+        if (moveHighScore && highScoreButton.transform.position.y < buttonGoal_pos)
+        {
+            whenClicked(highScoreButton);
+        }
+
+        if(startButton.transform.position.y >= buttonGoal_pos - 10)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+
+    private void whenClicked(Button selectedButton)
+    {
+        selectedButton.transform.position = new Vector3(selectedButton.transform.position.x, Mathf.Lerp(selectedButton.transform.position.y, buttonGoal_pos, 0.005f), selectedButton.transform.position.z);
+
+        selectedButton.transform.localScale = selectedButton.transform.localScale * Mathf.Lerp(1.0f, 0.5f, 0.005f);
+
+        selectedButton.transform.Rotate(new Vector3(0.0f, 0.0f, 1.0f));
+    }
+
+    private void StartButtonClick()
+    {
+        moveStart = true;
+    }
+
+    private void ExitButtonClick()
+    {
+        moveExit = true;
+    }
+
+    private void HighScoreButtonClick()
+    {
+        moveHighScore = true;
+    }
+}
